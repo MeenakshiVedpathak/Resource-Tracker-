f6a7f0af51d08584246708eb2fae561ea064ab87
poojagtt <pooja.honmore@gttdata.ai>
2026-08-06 15:03:05 +0530

    backend changes

--- backend/scripts/check_timesheet.js
+++
@@ -1,8 +0,0 @@
-try {
-  require('../src/services/timesheetService');
-  console.log('MODULE_OK');
-} catch (err) {
-  console.error('MODULE_ERROR');
-  console.error(err && err.stack ? err.stack : err);
-  process.exitCode = 2;
-}
